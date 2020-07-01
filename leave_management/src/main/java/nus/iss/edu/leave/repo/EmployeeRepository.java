4adeee2c42cef896ada7f17d3e01237561fdfda3
tesslyr <67495784+tesslyr@users.noreply.github.com>
2020-07-02 04:30:37 +0800

    Update EmployeeRepository.java

--- leave_management/src/main/java/nus/iss/edu/leave/repo/EmployeeRepository.java
+++ leave_management/src/main/java/nus/iss/edu/leave/repo/EmployeeRepository.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nus.iss.edu.leave.model.Employee;
+import nus.iss.edu.leave.model.Role;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	List<Employee> findAllEmployeesByName(String name);
 	
 	Employee findEmployeeByName(String name);
+	
+	List<Employee> findByRole(Role role);
 }
