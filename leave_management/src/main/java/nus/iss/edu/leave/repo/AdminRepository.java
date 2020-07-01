3fbe789cad6570a35b96e8ea0afac1e5e9e4558a
tesslyr <67495784+tesslyr@users.noreply.github.com>
2020-07-01 15:23:33 +0800

    Update AdminRepository.java

--- leave_management/src/main/java/nus/iss/edu/leave/repo/AdminRepository.java
+++ leave_management/src/main/java/nus/iss/edu/leave/repo/AdminRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import nus.iss.edu.leave.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
+        Optional<Admin> findByUsername(String username); 
 }
